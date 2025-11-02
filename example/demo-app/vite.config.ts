5806ffe6eb43c2b08e4f9aa53a5cfdbb599af3b0
Ilyas Ariba <ilyas.ariba@gmail.com>
2025-11-02 02:52:26 +0100

    fix

--- example/demo-app/vite.config.ts
+++ example/demo-app/vite.config.ts
@@ -1,5 +1,4 @@
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -7,11 +6,5 @@ export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/react-sensitive-hide/' : '/',
   server: {
     port: 3000,
-  },
-  resolve: {
-    alias: {
-      'react-sensitive-hide': resolve(__dirname, '../../dist'),
-      'react-sensitive-hide/styles.css': resolve(__dirname, '../../dist/index.css'),
-    },
   },
 });
