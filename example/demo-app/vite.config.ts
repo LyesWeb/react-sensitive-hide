9e79b73834abaf00032fbc1354dff44f6f1f1e83
Ilyas Ariba <ilyas.ariba@gmail.com>
2025-11-02 02:51:04 +0100

    fix demo app import issue

--- example/demo-app/vite.config.ts
+++ example/demo-app/vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/react-sensitive-hide/' : '/',
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      'react-sensitive-hide': resolve(__dirname, '../../dist'),
+      'react-sensitive-hide/styles.css': resolve(__dirname, '../../dist/index.css'),
+    },
   },
 });
